4428b731fb453aeccbdb7ae4343382840b8f6832
CJ <twoface01@gmail.com>
2022-06-17 00:26:29 +0000

    Fully styled Alpha Blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,6 +36,7 @@ class ArticlesController < ApplicationController
   
   def destroy
     @article.destroy
+    flash[:notice] = "Article was deleted successfully."
     redirect_to articles_path
   end
   
