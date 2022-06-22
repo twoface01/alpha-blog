004d3ba4b17a002cb73d522f9ef8541ec879c3a3
CJ <twoface01@gmail.com>
2022-06-22 20:41:36 +0000

    User ability to delete profile and all articles associated with their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 4)
@@ -39,6 +39,13 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles were deleted successfully."
+    redirect_to articles_path
+  end
+  
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
@@ -50,7 +57,7 @@ class UsersController < ApplicationController
     
     def require_same_user
       if current_user != @user
-        flash[:alert] = "You can only edit your own user profile."
+        flash[:alert] = "You can only edit or delete your own user profile."
         redirect_to @user
       end
     end
