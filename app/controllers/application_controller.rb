a65c3ede470ceda60387e54a740a37ab8d61a796
CJ <twoface01@gmail.com>
2022-06-22 19:21:26 +0000

    Users login and helper methods in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  
+  helper_method :current_user, :logged_in?
+  
+  def current_user
+    # return current user if it exists, otherwise query the database
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  def logged_in?
+    # bang bang turns the value to a boolean
+    !!current_user
+  end
+  
 end
